da90b3445fb01ffb21d66342cc97c004cb9b4872
Aldo Omar Morales Carlos <aomc06@gmail.com>
2025-04-04 19:41:01 -0500

    Diseño del form registro

--- app/src/main/java/com/aldomar/firebasesenati/RegistryActivity.kt
+++ app/src/main/java/com/aldomar/firebasesenati/RegistryActivity.kt
@@ -1,35 +1,20 @@
 package com.aldomar.firebasesenati
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class StartAppActivity : AppCompatActivity() {
-
-    private lateinit var buttonResgistry: Button
-
+class RegistryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_start_app)
+        setContentView(R.layout.activity_registry)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        buttonResgistry = findViewById(R.id.buttonResgistry)
-
-        buttonResgistry.setOnClickListener {
-            val intent = Intent(this, RegistryActivity::class.java)
-            Toast.makeText(this, "Registro", Toast.LENGTH_SHORT).show()
-            startActivity(intent)
-        }
-
     }
 }
