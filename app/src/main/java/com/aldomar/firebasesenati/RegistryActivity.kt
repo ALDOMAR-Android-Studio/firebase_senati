828d6bfe73e14a64dbbca126771fa3a76e9dcc04
Aldo Omar Morales Carlos <aomc06@gmail.com>
2025-04-04 20:09:52 -0500

    Validación del form registro

--- app/src/main/java/com/aldomar/firebasesenati/RegistryActivity.kt
+++ app/src/main/java/com/aldomar/firebasesenati/RegistryActivity.kt
@@ -1,12 +1,22 @@
 package com.aldomar.firebasesenati
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class RegistryActivity : AppCompatActivity() {
+
+    private lateinit var editTextUsername: EditText
+    private lateinit var editTextEmail: EditText
+    private lateinit var editTextPassword: EditText
+    private lateinit var editTextRepeatPassword: EditText
+    private lateinit var buttonSaveRegistry: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +26,45 @@ class RegistryActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        supportActionBar!!.title = "Registros"
+        initViews()
+        buttonSaveRegistry.setOnClickListener() {
+            if (validateInput()) {
+                Toast.makeText(this, "Registro exitoso", Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        }
     }
+
+    private fun initViews() {
+        editTextUsername = findViewById(R.id.editTextUsername)
+        editTextEmail = findViewById(R.id.editTextEmail)
+        editTextPassword = findViewById(R.id.editTextPassword)
+        editTextRepeatPassword = findViewById(R.id.editTextRepeatPassword)
+        buttonSaveRegistry = findViewById(R.id.buttonRegistry)
+    }
+
+    private fun validateInput(): Boolean {
+        val username = editTextUsername.text.toString()
+        val email = editTextEmail.text.toString()
+        val password = editTextPassword.text.toString()
+        val repeatPassword = editTextRepeatPassword.text.toString()
+
+        if (username.isEmpty()) {
+            Toast.makeText(this, "Ingrese su nombre de usuario", Toast.LENGTH_SHORT).show()
+        } else if (email.isEmpty()) {
+            Toast.makeText(this, "Ingrese su correo electrónico", Toast.LENGTH_SHORT).show()
+        } else if (password.isEmpty()) {
+            Toast.makeText(this, "Ingrese su contraseña", Toast.LENGTH_SHORT).show()
+        } else if (repeatPassword.isEmpty()) {
+            Toast.makeText(this, "Repita su contraseña", Toast.LENGTH_SHORT).show()
+        } else if (password != repeatPassword) {
+            Toast.makeText(this, "Las contraseñas no coinciden", Toast.LENGTH_SHORT).show()
+        } else {
+            return true
+        }
+        return false
+    }
+
+
 }
